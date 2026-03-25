9954498e01398e62805df31a131b79472c60e50e
shubh7705 <144321045+shubh7705@users.noreply.github.com>
2026-03-25 12:31:13 +0530

    Update DJANGO_SETTINGS_MODULE path in wsgi.py

--- WatchWise/WatchWise/wsgi.py
+++ WatchWise/WatchWise/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WatchWise.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WatchWise.WatchWise.settings')
 
 application = get_wsgi_application()
