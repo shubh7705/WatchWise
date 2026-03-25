ffd998bd2a597eaf80040b42f33ad3b8c0b99625
shubh7705 <144321045+shubh7705@users.noreply.github.com>
2026-03-25 12:45:36 +0530

    Update app name to 'WatchWise.accounts'

--- WatchWise/accounts/apps.py
+++ WatchWise/accounts/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+    name = 'WatchWise.accounts'
 
     def ready(self):
         import accounts.signals
