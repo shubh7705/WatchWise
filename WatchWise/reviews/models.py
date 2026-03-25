8c30e92d94d059b09b4e8974072664d06583b20a
shubh7705 <144321045+shubh7705@users.noreply.github.com>
2026-03-25 12:53:24 +0530

    Update import path for Movie model

--- WatchWise/reviews/models.py
+++ WatchWise/reviews/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
-from movies.models import Movie
+from WatchWise.movies.models import Movie
 
 
 class Review(models.Model):
