e035d0ddfd84af4508bb00bd6c6a86b011f6528c
shubh7705 <shubhamjadhav7705@gmail.com>
2026-09-01 21:39:11 +0530

    Remove unused assets and posters, register UserProfile in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+    search_fields = ("user__username", "bio")
